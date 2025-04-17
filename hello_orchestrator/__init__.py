a404a06910c3c8df317e2d79ef15ec5f73a98dc8
Ruka Sakurai <ruka.sakurai@gmail.com>
2025-04-17 13:53:49 +0900

    17 output orchestration instance id in logs (#18)

--- hello_orchestrator/__init__.py
+++ hello_orchestrator/__init__.py
@@ -1,7 +1,14 @@
 import azure.durable_functions as df
+import logging
 
 def orchestrator_function(context: df.DurableOrchestrationContext):
-    yield context.call_activity("say_hello", "Durable Functions")
-    return "Done"
+    instance_id = context.instance_id
+    logging.info(f"Orchestrator started. Instance ID: {instance_id}")
+    try:
+        yield context.call_activity("say_hello", "Durable Functions")
+        return "Done"
+    except Exception as e:
+        logging.error(f"Exception in orchestrator. Instance ID: {instance_id}. Error: {e}")
+        raise
 
 main = df.Orchestrator.create(orchestrator_function)
--- http_start/__init__.py
+++ http_start/__init__.py
@@ -1,7 +1,9 @@
 import azure.functions as func
 import azure.durable_functions as df
+import logging
 
 async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
     client = df.DurableOrchestrationClient(starter)
     instance_id = await client.start_new("hello_orchestrator")
+    logging.info(f"Started orchestration with instance ID: {instance_id}")
     return client.create_check_status_response(req, instance_id)
